670cf5f8d3cbc72b0d85866bc323852ab59b0ed2
Daniel Gomez <ss.im.back@gmail.com>
2019-10-09 21:05:55 -0700

    test fixed

--- test/api.example.test.js
+++ test/api.example.test.js
@@ -3,55 +3,62 @@ var chaiHttp = require("chai-http");
 var server = require("../server");
 var db = require("../models");
 var expect = chai.expect;
-
 // Setting up the chai http plugin
 chai.use(chaiHttp);
-
 var request;
-
-describe("GET /api/examples", function() {
+describe("GET /api/accounts", function() {
   // Before each test begins, create a new request server for testing
   // & delete all examples from the db
   beforeEach(function() {
     request = chai.request(server);
     return db.sequelize.sync({ force: true });
   });
-
-  it("should find all examples", function(done) {
+  it("should find all accounts", function(done) {
     // Add some examples to the db to test with
-    db.Example.bulkCreate([
-      { text: "First Example", description: "First Description" },
-      { text: "Second Example", description: "Second Description" }
+    db.Account.bulkCreate([
+      {
+        username: "First Example",
+        password: "First Password",
+        total: 120,
+        checking: 60,
+        savings: 60
+      },
+      {
+        username: "Second Example",
+        password: "Second Password",
+        total: 100,
+        checking: 60,
+        savings: 40
+      }
     ]).then(function() {
       // Request the route that returns all examples
-      request.get("/api/examples").end(function(err, res) {
+      request.get("/api/accounts").end(function(err, res) {
         var responseStatus = res.status;
         var responseBody = res.body;
-
         // Run assertions on the response
-
         expect(err).to.be.null;
-
         expect(responseStatus).to.equal(200);
-
         expect(responseBody)
           .to.be.an("array")
           .that.has.lengthOf(2);
-
         expect(responseBody[0])
           .to.be.an("object")
           .that.includes({
-            text: "First Example",
-            description: "First Description"
+            username: "First Example",
+            password: "First Password",
+            total: 120,
+            checking: 60,
+            savings: 60
           });
-
         expect(responseBody[1])
           .to.be.an("object")
           .that.includes({
-            text: "Second Example",
-            description: "Second Description"
+            username: "Second Example",
+            password: "Second Password",
+            total: 100,
+            checking: 60,
+            savings: 40
           });
-
         // The `done` function is used to end any asynchronous tests
         done();
       });
