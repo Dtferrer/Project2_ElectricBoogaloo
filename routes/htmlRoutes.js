4a3b0232055d649318d7db13fb4363119db3c971
Daniel Gomez <ss.im.back@gmail.com>
2019-10-09 20:36:14 -0700

    Configured HTTP Routes using express

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,23 +3,55 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    res.render("index", {
+      msg: "Welcome!"
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/login", function(req, res) {
+    res.render("index", {
+      msg: "Login"
     });
+  });
+
+  //accounts page
+  app.get("/accounts/:accountId", function(req, res) {
+    db.Account.findAll({
+      where: {
+        AccountId: req.params.accountId
+      }
+    })
+      .then(function(accounts) {
+        res.render("account-page", {
+          msg: "Hey!",
+          accounts: accounts
+        });
+      })
+      .catch(function(err) {
+        console.log(
+          "ERR - Failed to load Accounts for User ID: " + userId,
+          err
+        );
+        res.redirect("404");
+      });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/accounts/:accountId/shop", function(req, res) {
+    db.Purchase.findAll({ where: { AccountId: req.params.accountId } })
+      .then(function(purhases) {
+        res.render("shop", {
+          purhases: purhases
+        });
+      })
+      .catch(function(err) {
+        console.log("ERR - Failed to load purchases", err);
+        res.render("404");
+      });
+  });
+
+  app.get("/videos", function(req, res) {
+    res.render('videos');
   });
 
   // Render 404 page for any unmatched routes
