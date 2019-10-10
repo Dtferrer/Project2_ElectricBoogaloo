8f2594fb8ea9c09cf249e663f6d7fc8f6858bf2f
Danica Ferrer <dtferrer@ucsd.edu>
2019-10-09 20:13:29 -0700

    Finished basic routing with html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,38 +3,28 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
   // Load sign up page
   app.get("/signup", function(req, res) {
-    res.render("", {
-      stuff
-    });
-  })
+    res.render("signup");
+  });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  app.get("/user/:userId/accounts", function(req, res) {
+    db.Account.findOne({ where: { id: req.params.userIdd } }).then(function(
+      dbAccount
+    ) {
+      res.render("account-page", {
+        username: dbAccount
       });
     });
   });
 
   // Load shop page
   app.get("/shop", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("shop", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("shop");
   });
 
   // Render 404 page for any unmatched routes
